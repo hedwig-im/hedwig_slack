a57b9bdd7a1bb50a465a89a7556121fe2e8e421d
Sonny Scroggin <scrogson@gmail.com>
2016-03-23 15:40:46 -0500

    WIP

--- lib/hedwig_slack.ex
+++ lib/hedwig_slack.ex
@@ -1,3 +1,74 @@
 defmodule Hedwig.Adapters.Slack do
   use Hedwig.Adapter
+
+  alias Hedwig.Adapters.Slack.Connection
+
+  defmodule State do
+    defstruct conn: nil,
+              opts: nil,
+              robot: nil,
+              users: %{},
+              channels: %{}
+  end
+
+  def init({robot, opts}) do
+    {:ok, conn} = Connection.start_link(opts)
+    {:ok, %State{conn: conn, opts: opts, robot: robot}}
+  end
+
+  def handle_cast({:send, msg}, %{conn: conn} = state) do
+    Connection.ws_send(conn, slack_message(msg))
+    {:noreply, state}
+  end
+
+  def handle_cast({:reply, %{user: user, text: text} = msg}, %{conn: conn, users: users} = state) do
+    msg = %{msg | text: "<@#{user}|#{users[user]}>: #{text}"}
+    Connection.ws_send(conn, slack_message(msg))
+    {:noreply, state}
+  end
+
+  def handle_cast({:emote, msg}, %{conn: conn} = state) do
+    Connection.ws_send(conn, slack_message(msg))
+    {:noreply, state}
+  end
+
+  def handle_info(%{"type" => "message"} = msg, %{conn: conn, robot: robot} = state) do
+    msg = %Hedwig.Message{
+      adapter: {__MODULE__, self()},
+      ref: make_ref(),
+      room: msg["channel"],
+      text: msg["text"],
+      type: msg["type"],
+      user: msg["user"]
+    }
+    Hedwig.Robot.handle_message(robot, msg)
+    {:noreply, state}
+  end
+
+  def handle_info(%{"type" => "channel_created", "channel" => _channel}, state) do
+    {:noreply, state}
+  end
+
+  def handle_info(%{"type" => "channel_joined", "channel" => _channel}, state) do
+    {:noreply, state}
+  end
+
+  def handle_info(%{"type" => "message", "subtype" => "channel_join"}, state) do
+    {:noreply, state}
+  end
+
+  def handle_info({:user, %{"id" => id, "name" => name}}, %{users: users} = state) do
+    {:noreply, %{state | users: Map.put(users, id, name)}}
+  end
+
+  def handle_info(msg, state) do
+    IO.inspect msg
+    {:noreply, state}
+  end
+
+  defp slack_message(%Hedwig.Message{room: room, text: text, type: type}) do
+    %{channel: room,
+      text: text,
+      type: type}
+  end
 end
--- lib/hedwig_slack/connection.ex
+++ lib/hedwig_slack/connection.ex
@@ -1,7 +1,107 @@
 defmodule Hedwig.Adapters.Slack.Connection do
-  @behaviour Hedwig.Adapters.Connection
+  use GenServer
 
-  def connect(opts) do
-    # TODO: Connect!!!
+  @endpoint "https://slack.com/api/rtm.start"
+
+  defmodule State do
+    defstruct conn: nil,
+              host: nil,
+              next_id: 1,
+              owner: nil,
+              path: nil,
+              port: nil,
+              ref: nil,
+              token: nil,
+              query: %{},
+              server_data: %{},
+              reconnect_url: nil
   end
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, {self, opts})
+  end
+
+  def ws_send(pid, msg) do
+    GenServer.call(pid, {:ws_send, msg})
+  end
+
+  def init({owner, opts}) do
+    %URI{host: host, port: port, path: path, query: query} =
+      URI.parse(opts[:endpoint] || @endpoint)
+
+    {:ok, conn} = :gun.open(to_char_list(host), port)
+
+    :ok = GenServer.cast(self(), :rtm_start)
+
+    {:ok, %State{
+       conn: conn,
+       host: host,
+       owner: owner,
+       port: port,
+       path: path,
+       token: opts[:token],
+       query: query}}
+  end
+
+  def handle_call({:ws_send, msg}, _from, %{conn: conn, next_id: id} = state) do
+    msg = msg |> Map.put(:id, id) |> Poison.encode!()
+    :ok = :gun.ws_send(conn, {:text, msg})
+    {:reply, :ok, %{state | next_id: id + 1}}
+  end
+
+  def handle_cast(:rtm_start, %{conn: conn} = state) do
+    ref = :gun.get(conn, rtm_path(state))
+    {:ok, body} = :gun.await_body(conn, ref)
+    decoded = Poison.decode!(body)
+
+    for user <- decoded["users"] do
+      send(state.owner, {:user, user})
+    end
+
+    :ok = GenServer.cast(self(), :ws_upgrade)
+    {:noreply, %{state | ref: ref, server_data: decoded}}
+  end
+
+  def handle_cast(:ws_upgrade, %{conn: conn, server_data: %{"url" => url}} = state) do
+    %URI{host: host, path: path} = URI.parse(url)
+    :ok = :gun.close(conn)
+    {:ok, conn} = :gun.open(to_char_list(host), 443)
+    ref = :gun.ws_upgrade(conn, to_char_list(path))
+    {:noreply, %{state | conn: conn, ref: ref}}
+  end
+
+  def handle_info({:gun_response, _conn, _ref, _is_fin, _status, headers}, state) do
+    {:noreply, state}
+  end
+
+  def handle_info({:gun_ws_upgrade, _conn, :ok, _headers}, state) do
+    {:noreply, state}
+  end
+
+  def handle_info({:gun_ws, conn, {:text, data}}, %{conn: conn} = state) do
+    handle_data(data, state.owner)
+    {:noreply, state}
+  end
+
+  def handle_info({:gun_down, _conn, :http, :closed, [], []}, state) do
+    {:noreply, state}
+  end
+
+  def handle_info({:gun_up, _conn, :http}, state) do
+    {:noreply, state}
+  end
+
+  def handle_info(msg, state) do
+    IO.inspect msg
+    {:noreply, state}
+  end
+
+  defp handle_data(data, owner) do
+    send(owner, Poison.decode!(data))
+  end
+
+  defp rtm_path(%{path: path, query: nil, token: token}), do:
+    '#{path}?token=#{token}'
+  defp rtm_path(%{path: path, query: query, token: token}), do:
+    '#{path}?#{URI.encode_query(Map.put(query, token: token))}'
 end
