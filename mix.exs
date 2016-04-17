496028bce88240d010c6c60771b4412114f58838
Sonny Scroggin <scrogson@gmail.com>
2016-04-17 01:47:19 -0500

    Update hedwig to rc.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule HedwigSlack.Mixfile do
 
   def project do
     [app: :hedwig_slack,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -16,7 +16,7 @@ defmodule HedwigSlack.Mixfile do
 
   defp deps do
     [{:gun, "1.0.0-pre.1"},
-     {:hedwig, github: "hedwig-im/hedwig"},
+     {:hedwig, "~> 1.0.0-rc.4"},
      {:poison, "~> 2.0"}]
   end
 end
