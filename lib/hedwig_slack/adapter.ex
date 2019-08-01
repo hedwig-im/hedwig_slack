860d94de11b93ed9401d63db1c41fffa2f445c9b
Jeremy Neal <jneal1992@gmail.com>
2019-08-01 12:51:50 -0400

    Added support for sending interactive messages via send and reply.

--- lib/hedwig_slack/adapter.ex
+++ lib/hedwig_slack/adapter.ex
@@ -3,7 +3,7 @@ defmodule Hedwig.Adapters.Slack do
 
   require Logger
 
-  alias HedwigSlack.{Connection, RTM}
+  alias HedwigSlack.{Connection, RTM, ChatPostMessage}
 
   defmodule State do
     defstruct conn: nil,
@@ -25,13 +25,26 @@ defmodule Hedwig.Adapters.Slack do
   end
 
   def handle_cast({:send, msg}, %{conn: conn} = state) do
-    Connection.ws_send(conn, slack_message(msg))
+    case msg.text do
+      %{attachments: attachments} -> 
+        ChatPostMessage.post(state.token, msg.room, attachments)
+      _ ->
+        Connection.ws_send(conn, slack_message(msg))
+    end
     {:noreply, state}
   end
 
   def handle_cast({:reply, %{user: user, text: text} = msg}, %{conn: conn, users: _users} = state) do
-    msg = %{msg | text: "<@#{user.id}|#{user.name}>: #{text}"}
-    Connection.ws_send(conn, slack_message(msg))
+    case msg.text do
+      %{attachments: %{text: text} = attachments} ->
+        attachments = %{attachments | text: "<@#{user.id}|#{user.name}>: #{text}"}
+        ChatPostMessage.post(state.token, msg.room, attachments)
+      %{attachments: attachments} ->
+        ChatPostMessage.post(state.token, msg.room, attachments)
+      _ ->
+        msg = %{msg | text: "<@#{user.id}|#{user.name}>: #{text}"}
+        Connection.ws_send(conn, slack_message(msg))
+    end
     {:noreply, state}
   end
 
--- lib/hedwig_slack/chatPostMessage.ex
+++ lib/hedwig_slack/chatPostMessage.ex
@@ -1,7 +1,7 @@
 defmodule HedwigSlack.ChatPostMessage do
   alias HedwigSlack.HTTP
 
-  def post(token, channel, text, opts \\ []) do
-    HTTP.post("/chat.postMessage", body: %{token: token, channel: channel, text: text})
+  def post(token, channel, attachments, opts \\ []) do
+    {:ok, _resp} = HTTP.post("/chat.postMessage", body: %{channel: channel, attachments: attachments}, headers: ["Content-Type": "application/json", "Authorization": "Bearer #{token}"])
   end
 end
